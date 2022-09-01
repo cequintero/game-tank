3a427f75013bfd50c94c6100449a02bc6109e81c
Cesar Eduardo Quintero Rangel <cesar.quintero@neoris.com>
2022-08-31 21:49:28 -0500

    j

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/tanks_game
-echo Build started on `date`
-echo Compiling the Node.js code
-npm run build
-echo Build completed on `date`
+echo Server started on `date`
 pm2 start ./server/main.js
